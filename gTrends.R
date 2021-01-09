7712962c3683d02b1e1880178df68c12941273b4
paul tsagaroulis <22873425+ptsag@users.noreply.github.com>
2021-01-08 22:55:38 -0500

    Updated google trends through Dec 2020

--- gTrends.R
+++ gTrends.R
@@ -9,12 +9,14 @@ head(data); tail(data)
 
 gTrend<-ggplot() + geom_line(aes(x=year,y=cumsum(count),color='HR analytics'),data %>% filter(query=='HR Analytics')) +
   geom_line(aes(x=year,y=cumsum(count), color='People analytics'),data %>% filter(query=='People Analytics')) +
-  geom_line(aes(x=year,y=cumsum(count), color='Workforce analytics'),data %>% filter(query=='Workforce Analytics')) 
+  geom_line(aes(x=year,y=cumsum(count), color='Workforce analytics'),data %>% filter(query=='Workforce Analytics')) +
+  lims(x= c(2004, 2020), y = c(0, 6000))
   
 gTrendchart <- gTrend + 
-  ggtitle("Google searches (cumulative since 2014)") + 
+  ggtitle("'HR analytics' leads search interest (cumulative, by month since 2004)") + 
+  geom_point() + labs(caption = "Source: trends.google.com") +
   theme(
-  legend.position="bottom",   
+  legend.position="bottom",
   legend.title = element_blank(),
   axis.title.x = element_blank(),
   axis.title.y = element_blank()) 
